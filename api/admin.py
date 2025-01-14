0155219966041e89971512c3b12bbcc367aad6af
Luke Liew Mouawad <lukeliew9@gmail.com>
2025-01-14 22:43:17 +0000

    bug fixes to user creation

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User
+from .models import User, Hobby, UserHobby
 
 admin.site.register(User)
+admin.site.register(Hobby)
+admin.site.register(UserHobby)
